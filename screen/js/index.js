7e07990363b358b0ed56e6b4ad0e325c5112d263
HAYOON26 <69342163+HAYOON26@users.noreply.github.com>
2021-08-12 22:56:59 +0900

    bug: hayoon is the bug...
    죄송합니다ㅠㅜㅜㅠㅜㅠ

--- screen/js/index.js
+++ screen/js/index.js
@@ -1,12 +1,31 @@
-
 const nav = document.querySelector('.nav')
 
+// 스크롤링 색 변환 효과
 window.addEventListener('scroll', () => {
     if (window.scrollY > nav.getBoundingClientRect().bottom) {
-       console.log(nav.getBoundingClientRect().bottom, window.scrollY);
         nav.classList.add('down')
     } else {
         nav.classList.remove('down')
     }
 })
+
+// 네비게이션 메뉴 효과
+const navMenus = document.querySelectorAll('.nav-menu li')
+navMenus.forEach(navMenu => {
+    navMenu.addEventListener('click', (e) => {
+        e.preventDefault()
+        removeActive()
+        navMenu.classList.add('active')
+    })
+});
+
+function removeActive() {
+    navMenus.forEach(navMenu => {
+        navMenu.classList.remove('active')
+    });
+}
+
+
+
+
 
